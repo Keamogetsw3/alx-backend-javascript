a0c6e7b16139a61ae9d7a861f177044514fdc45d
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2025-01-18 21:25:44 +0200

    Update 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const { readFile } = require('fs');
 
 const app = express();
@@ -19,21 +20,25 @@ function countStudents(fileName) {
           if (lines[i]) {
             length += 1;
             const field = lines[i].toString().split(',');
-            if (field.length === 4 && field[3]) {  // Ensure valid line
-              if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-                students[field[3]].push(field[0]);
-              } else {
-                students[field[3]] = [field[0]];
-              }
-              fields[field[3]] = (fields[field[3]] || 0) + 1;
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
             }
           }
         }
         const l = length - 1;
         output += `Number of students: ${l}\n`;
         for (const [key, value] of Object.entries(fields)) {
-          output += `Number of students in ${key}: ${value}. `;
-          output += `List: ${students[key].join(', ')}\n`;
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
         }
         resolve(output);
       }
@@ -44,21 +49,15 @@ function countStudents(fileName) {
 app.get('/', (req, res) => {
   res.send('Hello ALX!');
 });
-
 app.get('/students', (req, res) => {
-  if (!process.argv[2]) {
-    res.send('This is the list of our students\nCannot load the database: No CSV file provided');
-    return;
-  }
-  countStudents(process.argv[2]).then((output) => {
+  countStudents(process.argv[2].toString()).then((output) => {
     res.send(['This is the list of our students', output].join('\n'));
-  }).catch((err) => {
-    res.send(`This is the list of our students\nCannot load the database: ${err.message}`);
+  }).catch(() => {
+    res.send('This is the list of our students\nCannot load the database');
   });
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
 });
 
 module.exports = app;
