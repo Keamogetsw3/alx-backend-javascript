cf8599d48ac037896e44eadef0cc0c76b1039405
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2025-01-20 21:58:45 +0200

    Update 4-update_grade_by_city.js
    reate a function updateStudentGradeByCity that returns an array of students for a specific city with their new grade
    
    It should accept a list of students (from getListStudents), a city (String), and newGrades (Array of “grade” objects) as parameters.
    
    newGrades is an array of objects with this format:
    
      {
        studentId: 31,
        grade: 78,
      }
    If a student doesn’t have grade in newGrades, the final grade should be N/A.
    
    You must use filter and map combined.

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,18 +1,11 @@
 export default function updateStudentGradeByCity(list, city, newGrade) {
   return list
-    .filter((obj) => obj.location === city)
+    .filter((student) => student.location === city)
     .map((student) => {
-      newGrade.map((studentGrade) => {
-        if (studentGrade.studentId === student.id) {
-          student.grade = studentGrade.grade;
-        }
-
-        if (!student.hasOwnProperty('grade')) {
-          student.grade = 'N/A';
-        }
-        return student;
-      });
-
-      return student;
+      const gradeInfo = newGrade.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeInfo ? gradeInfo.grade : 'N/A',
+      };
     });
 }
